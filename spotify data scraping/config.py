0588a967cc6f5cf0d59edf94de7c89936e6fcd57
Mohit Saini <63721227+msaini888@users.noreply.github.com>
2024-02-13 09:02:39 +0530

    changed config file

--- spotify data scraping/config.py
+++ spotify data scraping/config.py
@@ -1,5 +1,10 @@
-client_id = '575003a724fb4f64bebd10949606f9e5'
-client_secret = '9f70d62278184dbc929af2c840ad7195'
-username = 'a07wqhmlqmvs0lfjbckmgiizu'
-redirect_uri = 'http://localhost:7777/callback'
-scope = 'user-read-recently-played'
+#client_id = '575003a724fb4f64bebd10949606f9e5'
+#client_secret = '9f70d62278184dbc929af2c840ad7195'
+#username = 'a07wqhmlqmvs0lfjbckmgiizu'
+#redirect_uri = 'http://localhost:7777/callback'
+#scope = 'user-read-recently-played'
+
+import os
+
+client_id = os.environ.get('CLIENT_ID')
+print(client_id)
